c3663728d5afd35d0c099601687f8bc08274f841
amirowaisy72 <amirowaisy72@gmail.com>
2026-02-14 15:44:42 -0500

    update

--- app/AllContext/UsersContext.js
+++ app/AllContext/UsersContext.js
@@ -11,7 +11,7 @@ export function UsersProvider({ children }) {
   const host1offline = "http://localhost:3001/"
   const host2offline = "http://localhost:8000/"
   const host1online = "https://platform-axon-backend.vercel.app/"
-  const host2online = "https://known-emmaline-axon-1-26d7c97e.koyeb.app/"
+  const host2online = "https://irrelevant-andromache-axon-1-6410fc34.koyeb.app/"
 
   useEffect(() => {
     const stored = localStorage.getItem("user");
--- dashboard/app/AllContext/DashboardContext.jsx
+++ dashboard/app/AllContext/DashboardContext.jsx
@@ -16,7 +16,7 @@ export function DashboardProvider({ children }) {
   const host1offline = "http://localhost:3001/"
   const host2offline = "http://localhost:8000/"
   const host1online = "https://platform-axon-backend.vercel.app/"
-  const host2online = "https://known-emmaline-axon-1-26d7c97e.koyeb.app/"
+  const host2online = "https://irrelevant-andromache-axon-1-6410fc34.koyeb.app/"
 
   useEffect(() => {
     const eventSource = new EventSource(`${host2online}api/realtime-events`);
